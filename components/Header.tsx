91bc60ba193a0ab42721cdd6bae8dd16dcb69208
Vadimas Ko <kozemiakinas123@gmail.com>
2022-08-06 12:52:25 +0300

    add next/head to Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+import Head  from 'next/head'
 import styles from '../styles/components/Header.module.scss'
 
 interface Props {
@@ -9,6 +10,10 @@ interface Props {
 const Header = ({ title, description }: Props) => {
   return (
     <nav className={styles.header}>
+      <Head>
+        <title>{title}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <div className={styles.title}>
         <span>{'<'}Icon{'/>'}</span>
         <h1>{title}</h1>
